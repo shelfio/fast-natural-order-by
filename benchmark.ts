87d8ff2d633df54efacff3e5870bb17a9fe77c57
Yaroslav Chapelskyi <chapelskyi.slavik@gmail.com>
2023-11-15 11:36:15 +0200

    ci: don't cache deps

--- benchmark.ts
+++ benchmark.ts
@@ -1,4 +1,5 @@
 import benny from 'benny';
+// eslint-disable-next-line no-restricted-imports
 import {orderBy as orderByOriginal} from 'natural-orderby';
 import {orderBy as orderByOptimized} from './lib/index.js';
 
